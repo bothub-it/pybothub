91344cac88574dae2bbb5119662084f8e701eff8
Daniel Yohan <dyohan9@gmail.com>
2020-01-16 16:33:17 -0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name='bothub',
     version='1.0.0',
     packages=find_packages(),
+    url='https://github.com/bothub-it/pybothub',
     install_requires=[
         'requests >= 2.0.0, <= 2.22.0',
     ],
