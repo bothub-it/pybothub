1298969276921fc23a0cb9d3a5235cb993f3c807
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2020-01-19 21:02:14 -0300

    Added tests for method _request

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import requests_mock
 
 from bothub import Bothub
 
@@ -9,6 +10,19 @@ class BothubTestCase(unittest.TestCase):
 
     def test_get_url(self):
         self.assertEqual(self.bothub._get_url("/test"), f"{self.bothub._URL}/test")
+
+    @requests_mock.Mocker()
+    def test_request(self, request_mock):
+        url = self.bothub._get_url("test")
+        json = {"test": "test"}
+        request_mock.post(url, json=json)
+        request = self.bothub._request(
+            authorization="xxxxxxxxxx",
+            method="post",
+            path="test",
+            data={"test": "test"},
+        )
+        self.assertEqual(request, json)
 
 
 if __name__ == "__main__":
